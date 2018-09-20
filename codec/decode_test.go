66bbfa8ece6e30bdd45f3714c65d694b2d876e60
Frederick Akalin <akalin@keyba.se>
2018-09-20 13:55:24 -0700

    Fix test

--- codec/decode_test.go
+++ codec/decode_test.go
@@ -21,18 +21,19 @@ func doReadx(r decReader, n int) (i interface{}) {
 	return nil
 }
 
-func testBufioDecReaderReadx(t *testing.T, k uint) {
+func testBufioDecReaderReadx(t *testing.T, n int) {
 	var r bufioDecReader
 	r.buf = make([]byte, 0, 10)
 	r.reset(bytes.NewReader(nil))
-	i := doReadx(&r, 1<<k)
+	i := doReadx(&r, n)
 	if i != io.EOF {
-		t.Fatalf("(2<<%d) expected EOF, got %v", k, i)
+		t.Fatalf("(n=%d) expected EOF, got %v", n, i)
 	}
 }
 
 func TestBufioDecReaderReadx(t *testing.T) {
-	for k := uint(0); k < 31; k++ {
-		testBufioDecReaderReadx(t, k)
+	for n := 1; n < (1 << 10); n <<= 1 {
+		t.Logf("n=%d", n)
+		testBufioDecReaderReadx(t, n)
 	}
 }
